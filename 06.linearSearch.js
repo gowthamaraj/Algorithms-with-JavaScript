6cb5580b27871e17db90fef23c651fd6acde8640
gowthamaraj <gowthamaraj360@gmail.com>
2020-04-09 18:50:48 +0530

    modified: linear search

--- 06.linearSearch.js
+++ 06.linearSearch.js
@@ -7,7 +7,17 @@ function linerSearch(list,item){
     return -1
 }
 
-const result = linerSearch([1,2,7,4,5,12,10,3,9],4)
+const linerSearchForeach = (list,item)=>{
+    let index = -1
+    list.forEach((listItem,i)=>{
+        if(listItem === item){
+            index = i
+        }
+    })
+    return index
+}
+
+const result = linerSearchForeach([1,2,7,4,5,12,10,3,9],4)
 
 console.log('~~~~LinearSearch~~~~')
 if(result === -1){
